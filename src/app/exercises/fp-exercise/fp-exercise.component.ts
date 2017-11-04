5e70e7ffac835ff3445b9a6fd2d205e123f89fa1
Artieda Jorge (ERNI) <arj@erni-espana.es>
2017-11-04 10:57:12 +0100

    changes

--- src/app/exercises/fp-exercise/fp-exercise.component.ts
+++ src/app/exercises/fp-exercise/fp-exercise.component.ts
@@ -8,6 +8,23 @@ interface StockInfo {
     sector: string;
 }
 
+const getMaxValueElement = (x: StockInfo, y: StockInfo) => x.value >= y.value ? x : y;
+const maxValue = (stocks: StockInfo[]) => stocks.reduce((acc, cur) => getMaxValueElement(acc, cur));
+
+const max = comparer => (array: Array<any>) =>  array.reduce((acc, cur) => comparer(acc, cur) > 0 ? acc : cur);
+const maxValue2 = max((a: StockInfo, b: StockInfo) => a.value > b.value ? 1 : -1);
+
+// ex 3: {marketName -> Array stocks } 
+// const getMarketNameArray = marketName => (stoks: StockInfo[]) => getMarketNameObj;
+// const getMarketNameObj = (marketName, arr: StockInfo[]) => { marketName, arr };
+const filterByMarket = (marketName) => (stocks: StockInfo[]) => stocks.filter(stock => stock.market === marketName);
+const addToArrayIfNotExist = (arr: string[], element: StockInfo) => arr.find(x => x == element.market) ? arr : [...arr, element.market];
+const getSingleMaketArray = (stoks: StockInfo[]) => stoks.reduce((acc, cur) => addToArrayIfNotExist(acc, cur), []);
+
+// ex 4: order array of stocks by value
+const sortAsc = (a: StockInfo, b: StockInfo) => a.value - b.value;
+const sortStocks = (stocks: StockInfo[]) => [...stocks].sort(sortAsc);
+
 @Component({
     selector: 'fp-exercise',
     template: `<h2 class="subtitle">FP - Exercise</h2>
@@ -27,5 +44,16 @@ export class FpExerciseComponent implements OnInit {
 
     ngOnInit() {
         console.clear();
+
+        // get bigger value
+        // console.log("maxvalue");
+        // console.log(maxValue(stockData));
+        // console.log(maxValue2(stockData));
+
+        // ex3
+        // console.log(getSingleMaketArray(stockData));
+
+        // ex 4
+        console.log(sortStocks(stockData));
     }
 }
--- src/app/exercises/fp-introduction/fp-introduction.component.ts
+++ src/app/exercises/fp-introduction/fp-introduction.component.ts
@@ -13,6 +13,40 @@ const books: Array<Book> = [ { id: 1, title: "C# 6.0", author: "Andrew Troelsen"
     { id: 3, title: "Pro Angular 2", author: "Adam Freeman", rating: 4, releaseDate: 2015},
     { id: 4, title: "Pro ASP.NET", author: "Adam Freeman", rating: 2, releaseDate: 2001} ];
 
+const get = prop => x => x[prop];
+const getRating = get('rating');
+    
+// const composeOf2 = (fun1, fun2) => x => fun1(fun2(x));
+const compose = (...funcs) => x => funcs.reduce((f1, f2) => f2(f1), x);
+
+// exercise 2
+const getTitle = get('title');
+const getReleaseDate = get('releaseDate');
+const isEqual = value1 => value2 => value1 === value2;
+const isEqual2016 = isEqual(2016);
+const bookReleaseDateIs2016 = compose(getReleaseDate, isEqual2016);
+// const titleReleaseDateIs2016 = compose(bookReleaseDateIs2016, getTitle);
+
+const releaseDateIs2016FromBooks = (books: Book[]) => books.find(bookReleaseDateIs2016);
+const titleReleaseDateIs2016FromBooks = compose(releaseDateIs2016FromBooks, getTitle);
+
+// exercise 2: average
+// const averageBookRating = (books: Book[]) => 
+const fork = (join, func1, func2) => param => join(func1(param), func2(param));
+const divide = (a,b) => a/b;
+const sum = items => items.reduce((acc, cur) => acc + cur, 0);
+const meanSimplified = items => items.reduce((acc, cur) => acc + cur, 0) / items.length;
+const averageBookRating = books => compose(getRating, meanSimplified);
+const count = items => items.length;
+const mean = fork(divide, sum, count);
+
+// ex 3: stars
+const writeStars = (number: number) => new Array(number).reduce((acc, cur) => acc + "*", "");
+const writeBookStars = (book: Book) => `${book.title}: ${writeStars(book.rating)}`;  
+const writeBooksStars = (books: Book[]) => books.forEach(writeBookStars);
+const print = value => console.log(value);
+const printBookStars = compose(writeBookStars, print);
+
 @Component({
     selector: 'fp-introduction',
     template: `<h2 class="subtitle">Function Concepts</h2>
@@ -56,5 +90,20 @@ books: Array<Book> = [ { id: 1, title: "C# 6.0", author: "Andrew Troelsen", rati
 
     ngOnInit() {
         console.clear();
+
+        // Exercise 1
+        // books.forEach((book) => console.log(getRating(book)));
+
+        // Exercise 2
+        // console.log("title");
+        // console.log(titleReleaseDateIs2016FromBooks(books));
+
+        // Exercise 2: average book rating
+        // console.log(averageBookRating(books));
+
+        // Ex 3
+        // books.map(printBookStars);
+        // writeBooksStars(books);
     }
+
 }
